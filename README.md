485b2720a147c60d22e80ba598593845b60329d9
bruce.tseng <bruce.tseng@bytedance.com>
2021-12-27 20:36:19 +0800

    init
    Change-Id: I6c5d1b72b773ec1241eab670404b7286188fbb81

---
+++ README.md
@@ -0,0 +1,2 @@
+## Srock Site UI (TailwindCSS) BruceFE
+- 初始空專案
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CompressionPlugin = require("compression-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  target: 'web',
+  // 入口
+  entry: './src/index.js',
+  // 模式 development
+  mode: 'development',
+  // 出口
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.[hash].js',
+  },
+  // loader
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          }
+        ],
+      },
+      {
+        test: /\.gif/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        }
+      }
+    ],
+  },
+  // 插件
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'index.[hash].css'
+    }),
+    new CompressionPlugin()
+  ],
+
+  devtool: 'source-map'
+}
