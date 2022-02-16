d422ffa4046d3f3885f98de25baa3cb6d696b20b
steven <steven_ko@paradise-soft.com.tw>
2022-02-16 18:04:40 +0800

    feat: array tuble元祖

---
+++ src/ch3_3.ts
@@ -0,0 +1,17 @@
+const arr1 = [1,2,3]
+
+const arr2 = [1,2,'3']
+
+const arr3: string[] = []
+// arr3.push(300)
+
+const arr4: Array<string> = []
+
+const arr5: (string|boolean)[] = []
+// arr5.push('123')
+// arr5.push(false)
+// arr5.push(123)
+
+// Tuple
+const tu1: [number, string, boolean] = [123, '123', false]
+const tu2: [number, number][] = [[1,2], [3,4]]
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 import "./index.css";
+import './ch3_3'
